192c0b443c809459705d936ead5bafd90b21fb26
dimasluke-swiftknight <93350204+dimasluke-swiftknight@users.noreply.github.com>
2021-12-16 21:28:32 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 const configs = require('./api/config/index');
 
-const { logger } = require('./api/config/index');
+const { logger } = configs;
 
 global.logger = logger;
 global.applicationName = 'template-api';
