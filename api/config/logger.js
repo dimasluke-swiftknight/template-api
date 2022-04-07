1212a00f3b8bcb2e54051b7e4fa97c5332b9da3c
Luke Dimas <dimal@swift.com>
2022-04-07 13:50:05 -0700

    update logger add elastic config/ update dependencies

--- api/config/logger.js
+++ api/config/logger.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 const winston = require('winston');
+const { ElasticsearchTransport } = require('winston-elasticsearch');
 require('winston-daily-rotate-file');
 require('winston-mongodb');
 
@@ -13,30 +14,48 @@ const {
 } = format;
 
 const customFormat = format((logEntry) => ({
+  service: {
+    name: 'replace_me',
+    node: {
+      name: process.env.HOSTNAME || 'replace_me-default',
+    },
+  },
   ...logEntry,
-  metadata: {
-    ...logEntry.metadata,
+  metaData: {
+    ...logEntry.metaData,
     ...{ application, environment },
   },
 }));
 
-const mongodbTransport = new winston.transports.MongoDB({
-  db: process.env.MONGODB_CONNECTION_STRING_LOGS || 'mongodb://127.0.0.1:27017',
-  collection: 'dev-logs',
-  level: 'info',
-  options: {
-    useUnifiedTopology: true,
+const elasticsearchTransport = new ElasticsearchTransport({
+  index: process.env.ELASTICSEARCH_INDEX,
+  clientOpts: {
+    node: process.env.ELASTICSEARCH_NODE,
+    auth: {
+      username: process.env.ELASTICSEARCH_AUTH_USERNAME,
+      password: process.env.ELASTICSEARCH_AUTH_PASSWORD,
+    },
   },
-  capped: true,
+  level: process.env.ELASTICSEARCH_LOG_LEVEL,
 });
 
-const fileTransport = new transports.DailyRotateFile({
-  filename: './logs/application-%DATE%.log',
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-});
+// const mongodbTransport = new winston.transports.MongoDB({
+//   db: process.env.MONGODB_CONNECTION_STRING_LOGS || 'mongodb://127.0.0.1:27017',
+//   collection: 'dev-logs',
+//   level: 'info',
+//   options: {
+//     useUnifiedTopology: true,
+//   },
+//   capped: true,
+// });
+
+// const fileTransport = new transports.DailyRotateFile({
+//   filename: './logs/application-%DATE%.log',
+//   datePattern: 'YYYY-MM-DD-HH',
+//   zippedArchive: true,
+//   maxSize: '20m',
+//   maxFiles: '14d',
+// });
 
 const logConfiguration = {
   level: process.env.LOG_LEVEL || 'info',
@@ -47,8 +66,9 @@ const logConfiguration = {
     prettyPrint(),
   ),
   transports: [
-    fileTransport,
-    mongodbTransport,
+    // fileTransport,
+    // mongodbTransport,
+    elasticsearchTransport,
   ],
 };
 
--- global.js
+++ global.js
@@ -1,9 +1,4 @@
 require('dotenv').config();
 
-const configs = require('./api/config/index');
-
-const { logger } = configs;
-
-global.logger = logger;
 global.environment = process.env.APPLICATION_ENV || 'local';
-global.application = 'template-api';
+global.application = 'replace_me';
